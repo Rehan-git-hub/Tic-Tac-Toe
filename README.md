12d095bcea8bf41b49efcc2bfce9a630e6c69bf4
Rehan Shaikh <rehan0edu123@gmail.com>
2025-04-07 00:26:10 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tic Tac Toe
+
+A simple 2-player Tic Tac Toe game using HTML, CSS, and JavaScript.
+
+## How to Play
+
+- Two players take turns clicking on the grid.
+- The first to get 3 in a row wins (horizontal, vertical, or diagonal).
+
+## How to Run
+
+Just open the index.html file in your browser.
+
+---
+
+Made for learning and practice.
---
+++ script.js
@@ -0,0 +1,77 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset");
+let turn0 = true;
+let message = document.querySelector(".message");
+let newGame = document.querySelector(".newgame");
+let messageBox = document.querySelector(".message-box");
+let clickcounter = 0;
+const winning=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        clickcounter++;
+        if(turn0) {
+            box.innerText = "O";
+            turn0 = false;
+        }else{
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true ;
+        checkWinner();
+    });
+});
+const showWinner = (winner) => {
+    message.innerText = `Winner is ${winner}`
+    messageBox.classList.remove("hide");
+    clickcounter=0;
+}
+const showDraw = () => {
+    message.innerText = `The match was a draw`
+    messageBox.classList.remove("hide");
+    clickcounter=0;
+}
+
+const checkWinner = () => {
+    for(let pattern of winning){
+        // console.log(boxes[pattern[0]].innerText , boxes[pattern[1]].innerText , boxes[pattern[2]].innerText);
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if(pos1 != "" && pos2 != "" && pos3 != ""){
+            if(pos1===pos2 && pos2===pos3){
+                for(box of boxes){
+                    box.disabled = true;
+                }
+                showWinner(pos1);
+            }
+            else if(clickcounter>=9){
+                showDraw();
+            }
+        }
+    }
+};
+const resetgame = () => {
+    turn0 = true;
+    enableBoxes();
+    messageBox.classList.add("hide");
+}
+const enableBoxes = () => {
+    for(box of boxes){
+        box.disabled = false;
+        box.innerText= "";
+    }
+}
+newGame.addEventListener("click", resetgame);
+resetbtn.addEventListener("click", resetgame);
